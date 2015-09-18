efc1477dcd01e8a4a05068a56faa6fdc4dcb55e7
Marhek <vdheidenMark@gmail.com>
2015-09-18 12:13:28 +0200

    Revert "Added square"
    This reverts commit 2633b4ed8d23664d5ef5f65fb810dcd2fd10e53d.

--- Triangle/Triangle/Triangle.py
+++ Triangle/Triangle/Triangle.py
@@ -1,9 +1,1 @@
-﻿size = int(raw_input("Enter a number: "))
-
-for i in range(size, 1, -1):
-    if i == size:
-        print (i*"*")+ (" "*(size-(i+1)))
-    else:
-        print (i*"*")+ (" "*(size-(i+1)))+("*")
-
-print size*"*"
+Aantal = raw_input("Voer een getal in ")
--- Triangle/square/square.py
+++
@@ -1,7 +0,0 @@
-size = int(raw_input("Enter a number: "))
-
-for i in range(size, 0, -1):
-    if i == size or i == 0:
-        print size*"*"
-    else:
-        print "*"+(i-2)*" "+"*"
