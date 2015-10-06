f3ba1229e70733512e7efd1d4d4e8dad5652ac22
Marhek <vdheidenMark@gmail.com>
2015-10-06 23:36:21 +0200

    Changed Circle

---
+++ Circle/Circle/Circle.py
@@ -0,0 +1,18 @@
+﻿from math import *
+
+size = int(raw_input("Voer een getal in: "))
+string = ""
+
+for i in range(0,size*2):
+    for j in range(0,size*2):
+        di = (i - size)**2
+        dj = (j - size)**2
+        dist = sqrt(di + dj)
+
+        if size > dist:
+            string += "*"
+        else:
+            string += " "
+
+    string += "\n"
+print string
