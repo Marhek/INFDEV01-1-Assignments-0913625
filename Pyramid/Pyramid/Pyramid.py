f4684b08070387c824b1d18c6e73ef06295c0f6b
Marhek <vdheidenMark@gmail.com>
2015-10-07 14:25:38 +0200

    Changes

--- Pyramid/Pyramid/Pyramid.py
+++ Pyramid/Pyramid/Pyramid.py
@@ -1,7 +1,10 @@
-﻿size = int(raw_input("Voer een getal in: "))
+﻿size = int(raw_input("Voer een onevengetal in: "))
 string = ""
 
-for i in range(1,size+1,+2):
-    string += (size-i)/2*" "+i*"*"+"\n"
-
+for i in range(1,size+1,2):
+    for s in range(size/2,-1,-1):
+        string += " "
+    for j in range(i):
+        string += "*"
+    string += "\n"
 print string
